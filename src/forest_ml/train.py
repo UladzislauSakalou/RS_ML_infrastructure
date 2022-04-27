7d616efb76da2b52496a0e2d7f9da41f70e75601
Uladzislau Sakalou <Uladzislau_Sakalou@epam.com>
2022-04-27 19:31:25 +0300

    add test_split_ratio option

--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -26,17 +26,23 @@ from joblib import dump
     default=42,
     type=int
 )
+@click.option(
+    "--test-split-ratio",
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+)
 def train(
     dataset_path: Path,
     save_model_path: Path,
-    random_state: int
+    random_state: int,
+    test_split_ratio: float
 ):
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop('Cover_Type', axis=1)
     target = dataset['Cover_Type']
     features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=0.2, random_state=random_state
+        features, target, test_size=test_split_ratio, random_state=random_state
     )
     classifier = RandomForestClassifier(random_state=random_state).fit(features_train, target_train)
     accuracy = accuracy_score(target_val, classifier.predict(features_val))
