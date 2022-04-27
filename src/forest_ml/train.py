020c07b59e2a183c5916d32e7ac9c4118673afc8
Uladzislau Sakalou <Uladzislau_Sakalou@epam.com>
2022-04-27 18:03:28 +0300

    add saving model after train

--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -4,6 +4,7 @@ import click
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from joblib import dump
 
 
 @click.command()
@@ -14,7 +15,13 @@ from sklearn.ensemble import RandomForestClassifier
     help='Path to the dataset',
     type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
-def train(dataset_path: Path):
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path)
+)
+def train(dataset_path: Path, save_model_path: Path):
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop('Cover_Type', axis=1)
@@ -25,3 +32,5 @@ def train(dataset_path: Path):
     classifier = RandomForestClassifier(random_state=42).fit(features_train, target_train)
     accuracy = accuracy_score(target_val, classifier.predict(features_val))
     click.echo(f"Accuracy: {accuracy}.")
+    dump(classifier, save_model_path)
+    click.echo(f"Model is saved to {save_model_path}.")
