395965d9ce246fe550a06ca29d1d5ccd653f274d
Uladzislau Sakalou <Uladzislau_Sakalou@epam.com>
2022-05-05 21:12:27 +0300

    add nox

---
+++ noxfile.py
@@ -0,0 +1,71 @@
+import tempfile
+from typing import Any
+import nox
+from nox.sessions import Session
+
+
+nox.options.sessions = "black", "mypy", "tests"
+locations = "src", "tests", "noxfile.py"
+temp_file_name = "temp.txt"
+import os
+
+
+# def install_with_constraints(session, *args, **kwargs) -> None:
+#     with tempfile.NamedTemporaryFile(delete=True) as requirements:
+#         session.run(
+#             "poetry",
+#             "export",
+#             "--dev",
+#             "--format=requirements.txt",
+#             "--without-hashes",
+#             f"--output={requirements.name}",
+#             external=True,
+#         )
+#         session.install(f"--constraint={requirements.name}", *args, **kwargs)
+#     # session.install(f"--constraint=temp.txt", *args, **kwargs)
+
+
+def install_with_constraints(session, *args, **kwargs) -> None:
+    session.run(
+        "poetry",
+        "export",
+        "--dev",
+        "--format=requirements.txt",
+        "--without-hashes",
+        f"--output={temp_file_name}",
+        external=True,
+    )
+    session.install(f"--constraint={temp_file_name}", *args, **kwargs)
+    os.unlink(temp_file_name)
+    # session.install(f"--constraint=temp.txt", *args, **kwargs)
+
+
+@nox.session(python="3.9")
+def black(session: Session) -> None:
+    """Run black code formatter."""
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+
+
+@nox.session(python="3.9")
+def mypy(session: Session) -> None:
+    """Type-check using mypy."""
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
+
+
+@nox.session(python="3.9")
+def tests(session: Session) -> None:
+    """Run the test suite."""
+    args = session.posargs
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "pytest")
+    session.run("pytest", *args)
+
+
+# @nox.session(python="3.9")
+# def tests(session):
+#     session.run("poetry", "install", external=True)
+#     session.run("pytest")
